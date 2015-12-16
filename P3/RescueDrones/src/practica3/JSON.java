43f4226eaf04bdd69742658c1174c28f6db3f98e
Jogua <josegua93@gmail.com>
2015-12-16 14:06:21 +0100

    JSON

--- P3/RescueDrones/src/practica3/JSON.java
+++ P3/RescueDrones/src/practica3/JSON.java
@@ -1,44 +1,88 @@
 package practica3;
 
 import com.eclipsesource.json.*;
+import java.awt.Point;
 
 public class JSON {
 
     private static String key;
 
     private static JsonObject parseToJson(String json) {
-	throw new UnsupportedOperationException();
+	return Json.parse(json).asObject();
     }
 
     public static String suscribirse(String mundo) {
-	throw new UnsupportedOperationException();
+	JsonObject object = new JsonObject();
+	object.add("world", mundo);
+	return object.toString();
     }
 
     public static void guardarKey(String json) {
-	throw new UnsupportedOperationException();
+	JsonObject object = Json.parse(json).asObject();
+	key = object.getString("result", null);
     }
 
     public static String checkin() {
-	throw new UnsupportedOperationException();
+	JsonObject object = new JsonObject();
+	object.add("comand", "checkin");
+	object.add("key", key);
+	return object.toString();
     }
 
     public static Rol getRol(String json) {
-	throw new UnsupportedOperationException();
+	JsonObject object = parseToJson(json);
+	int rol = object.getInt("rol", -1);
+	return Rol.getRol(rol);
     }
 
     public static String mover(String movimiento) {
-	throw new UnsupportedOperationException();
+	JsonObject object = new JsonObject();
+	object.add("comand", movimiento);
+	object.add("key", key);
+	return object.toString();
     }
 
     public static String repostar() {
-	throw new UnsupportedOperationException();
+	JsonObject object = new JsonObject();
+	object.add("comand", "refuel");
+	object.add("key", key);
+	return object.toString();
     }
 
     public static String key() {
-	throw new UnsupportedOperationException();
+	JsonObject object = new JsonObject();
+	object.add("key", key);
+	return object.toString();
     }
 
     public static Percepcion getPercepcion(String json) {
-	throw new UnsupportedOperationException();
+	JsonObject object = parseToJson(json);
+	Percepcion percepcion = new Percepcion();
+	JsonObject result = object.get("result").asObject();
+	percepcion.setBateria(result.getInt("battery", -1));
+	Point gps = new Point();
+	gps.x = result.getInt("x", -1);
+	gps.y = result.getInt("y", -1);
+	percepcion.setGps(gps);
+	int[][] radar = parseSensor(result.get("sensor").asArray());
+	percepcion.setRadar(radar);
+	percepcion.setEnergia(result.getInt("energy", -1));
+	percepcion.setLlegado(result.getBoolean("goal", false));
+	return percepcion;
+    }
+
+    private static int[][] parseSensor(JsonArray array) {
+	int tam = (int) Math.sqrt(array.size());
+	int[][] sensor = new int[tam][tam];
+	int i = 0, j = 0;
+	for (JsonValue value : array) {
+	    sensor[i][j] = value.asInt();
+	    j++;
+	    if (j == tam) {
+		j = 0;
+		i++;
+	    }
+	}
+	return sensor;
     }
 }
